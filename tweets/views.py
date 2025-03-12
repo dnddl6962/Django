4d56dd028c1310509004a61c29e1b1b484018634
dnddl6962 <77288155+dnddl6962@users.noreply.github.com>
2025-03-12 16:22:32 +0900

    Urls, Views and Templates

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Tweet
 
 # Create your views here.
+
+
+# request = 누가 요청했는지, 어떤 데이터가 전송되고 있는지?
+def see_all_tweets(request):
+    tweets = Tweet.objects.all()
+    return render(
+        request,
+        "all_tweets.html",
+        {
+            "tweets": tweets,
+        },
+    )
+
+
+def see_one_tweet(request, tweet_pk):
+    try:
+        tweet = Tweet.objects.get(pk=tweet_pk)
+        return render(
+            request,
+            "one_tweet.html",
+            {"tweet": tweet},
+        )
+    except Tweet.DoesNotExist:
+        return render(
+            request,
+            "one_tweet.html",
+            {
+                "not_found": True,
+            },
+        )
